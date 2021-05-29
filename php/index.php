ea572d00f752a213332a10e2e8bb8922b7ccaf48
programmingmuscle <programmingmuscle@gmail.com>
2021-05-29 16:55:10 +0900

    second git
    勉強のため

--- php/index.php
+++ php/index.php
@@ -1,20 +1,5 @@
 <?php  
-    echo null;
-    var_dump("10 ");
-
-    $array = array(
-        'fugo',
-    );
-    $array[] = 'hoge';
-
-    var_dump($array);
-    
-    
-    $array=[
-        "h",
-        "f",
-    ];
-    var_dump($array);
+    echo 'first git'
 
 
 
